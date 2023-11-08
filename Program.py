8d80c2c7d0e27af4b9be4ae300ed31c220d501b1
yadavbikash5152 <140582027+yadavbikash5152@users.noreply.github.com>
2023-11-08 21:03:10 +0545

    Add files via upload

---
+++ Program.py
@@ -0,0 +1,25 @@
+#1
+print("Hello World!")
+
+#2
+print("Hello Bikashanand")
+
+#3
+C=38.4
+F=C*(9/5)+32
+print(C)
+print(F)
+
+#4
+run=48426
+batted_times=1014
+not_out=162
+innings=batted_times - not_out
+batting_avg=run/innings
+print(batting_avg)
+
+#5
+lab_group=24
+group_first=113//lab_group
+group_first1=113%lab_group
+print("num of group: ",group_first,"remainder: ",group_first1)
